ca8d969f2e032e1b7e1b4bc148b0cf3eeb8f3aef
breezy-desk-5482 <rmcclorey1@gmail.com>
2020-04-26 00:11:21 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,7 +1,7 @@
 def find_min_value(array)
   num = 0
   min_value = 1000000
-  while num < array.length
+  while num < array.length do
   if array[num] < min_value
     min_value = array[num]
   end
